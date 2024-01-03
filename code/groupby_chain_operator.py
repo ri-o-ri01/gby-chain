25c3f74432cd2b2695cec1e0705207fc4f48cf86
ri-o-ri01 <ryo.ishi2012@gmail.com>
2024-01-03 20:33:21 +0900

    [refactor]extract common functions

--- code/groupby_chain_operator.py
+++ code/groupby_chain_operator.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import re
@@ -7,106 +6,91 @@ from gbychain.code.groupby_result import GroupbyResult
 
 
 class GroupbyChainOperator:
-
     def __init__(self, init_df, init_name="init"):
         if init_df is None:
             raise ValueError("Please set init_df")
 
-        # self.agg_tf_result_dict = {}
         self.init_df = init_df
         self.init_name = init_name
         self.level = 0
-        self.level_result_dict = {}
-        self.level_result_dict[str(self.level)] = GroupbyResult(self.init_name, "None", None, None, None, None, None,
-                                                                None, init_df, None, self.level)
+        self.level_result_dict = {str(self.level): GroupbyResult(self.init_name, "None",
+                                                                 None, None, None,
+                                                                 None, None, None,
+                                                                 init_df, None, self.level)}
         self.level_column_dict = {}
-        # self.agg_tf_result_dict2 = {}
 
-    def update_by_agg(self, groups, indeces=None,
-                      tgt_names=None, func_dict=None, funcs_name=None, output_name=None, is_debug=False):
-
+    def check_update_inputs(self, tgt_names=None, func_dict=None):
         prev_level = self.level
         input_result = self.level_result_dict[str(prev_level)]
         input_df = input_result.output_df
         input_name = input_result.output_name
+
         if tgt_names is None:
             tgt_names = input_result.tgt_names
         if func_dict is None:
             func_dict = input_result.func_dict
         if tgt_names is None or func_dict is None:
-            raise ValueError("Please set func_dict and func_dict as they are not included in the previous result.")
+            raise ValueError("Please set tgt_names and func_dict as they are not included in the previous result.")
 
-        # print(prev_level)
-        # print(self.level_result_dict)
+        return input_df, input_name, tgt_names, func_dict
 
+    def update_level_result(self, new_df, operation_type, input_name, groups, indices, tgt_names, func_dict, funcs_name,
+                            columns_dict, output_name):
+        level = self.level + 1
+        result = GroupbyResult(output_name, operation_type, input_name, groups, indices, tgt_names, func_dict,
+                               funcs_name, new_df, columns_dict, level)
+        self.level_column_dict[str(level)] = columns_dict
+        self.level_result_dict[str(level)] = result
+        self.level = level
+
+    def update_by_agg(self, groups, indeces=None, tgt_names=None, func_dict=None, funcs_name=None, output_name=None,
+                      is_debug=False):
+        input_df, input_name, tgt_names, func_dict = self.check_update_inputs(tgt_names, func_dict)
 
         agg_df, columns_dict = agg_by_groups(input_df, groups, tgt_names, func_dict, True, is_debug=is_debug)
         if is_debug:
             print(tgt_names)
 
         gp_inx_names = groups + indeces if indeces is not None else groups
-        # tgt_names2 = agg_df.drop(gp_inx_names, axis=1, errors='ignore').columns
         tgt_names2 = list(columns_dict.keys())
-        level = self.level + 1
-        result = GroupbyResult(output_name, "agg", input_name, groups, indeces, tgt_names2, func_dict, funcs_name,
-                               agg_df, columns_dict, level)
-        self.level_column_dict[str(level)] = columns_dict
-        self.level_result_dict[str(level)] = result
-        self.level = level
+        self.update_level_result(agg_df, "agg", input_name, groups, indeces, tgt_names2, func_dict,
+                                 funcs_name, columns_dict, output_name)
         return self
 
-    def update_by_apply2_agg(self, groups,
-                             indices=None, tgt_names_func_list=None, output_name=None):
+    def update_by_apply2_agg(self, groups, indices=None, tgt_names_func_list=None, output_name=None):
         prev_level = self.level
         input_result = self.level_result_dict[str(prev_level)]
         input_df = input_result.output_df
         input_name = input_result.output_name
 
-        tgt_names = [s[0] for s in tgt_names_func_list]
-        funcs = [s[2] for s in tgt_names_func_list]
+        # tgt_names = [s[0] for s in tgt_names_func_list]
+        # funcs = [s[2] for s in tgt_names_func_list]
         apply_df, columns_dict = apply_agg_by_groups(input_df, groups, tgt_names_func_list, True)
         tgt_names2 = apply_df.drop(groups, axis=1).columns
-        level = self.level + 1
-        result = GroupbyResult(output_name, "apply2-agg", input_name, groups, indices, tgt_names2, None, None, apply_df,
-                               columns_dict, level)
-        self.level_column_dict[str(level)] = columns_dict
-        self.level_result_dict[str(level)] = result
-        self.level = level
+        self.update_level_result(apply_df, "apply2-agg", input_name, groups, indices, tgt_names2,
+                                 None, None,
+                                 columns_dict, output_name)
         return self
 
-    def update_by_transform(self, groups,
-                            indices=None, tgt_names=None, func_dict=None, funcs_name=None, output_name=None,
-                            is_debug=False):
-        prev_level = self.level
-        input_result = self.level_result_dict[str(prev_level)]
-        input_df = input_result.output_df
-        input_name = input_result.output_name
-        if tgt_names is None:
-            tgt_names = input_result.tgt_names
-        if func_dict is None:
-            func_dict = input_result.func_dict
-        if tgt_names is None or func_dict is None:
-            raise ValueError("Please set func_dict and func_dict as they are not included in the previous result.")
+    def update_by_transform(self, groups, indices=None, tgt_names=None, func_dict=None, funcs_name=None,
+                            output_name=None, is_debug=False):
+        input_df, input_name, tgt_names, func_dict = self.check_update_inputs(tgt_names, func_dict)
 
         tf_df, columns_dict = tf_by_groups(input_df, groups, tgt_names, func_dict, True)
-        # gp_inx_names = groups + indices if indices is not None else groups
-        # tgt_names2 = tf_df.drop(gp_inx_names, axis=1, errors='ignore').columns
         tf_df = tf_df.drop(tgt_names, axis=1)
         tgt_names2 = list(columns_dict.keys())
         if is_debug:
             print(tgt_names2)
-        level = self.level + 1
-        result = GroupbyResult(output_name, "transform", input_name, groups, indices, tgt_names2, func_dict, funcs_name,
-                               tf_df, columns_dict, level)
-        self.level_column_dict[str(level)] = columns_dict
-        self.level_result_dict[str(level)] = result
-        self.level = level
+        self.update_level_result(tf_df, "transform", input_name, groups, indices, tgt_names2, func_dict,
+                                 funcs_name, columns_dict, output_name)
         return self
+
+    # ... [他のメソッドや補助関数]
 
     def get_output_df(self, level, use_func_order=True):
         result = self.level_result_dict[str(level)]
         output_df = result.output_df.copy()
-        if use_func_order :
+        if use_func_order:
             return output_df
         else:
             if level == 0:
@@ -138,39 +122,27 @@ class GroupbyChainOperator:
                     [ret_df, output_df.drop(groups, axis=1)],
                     axis=1)
             else:
-                ret_df  = ret_df.merge(output_df, on = groups, how = "left")
+                ret_df = ret_df.merge(output_df, on=groups, how="left")
         return ret_df
 
     def get_log(self):
         return [str(v) for _, v in self.level_result_dict.items()]
 
 
-
 def apply_agg_by_groups(df, groups, col2_func_tpl_list, use_func_order=False
                         ):
-    """
-    DataFrameに対して指定されたグループとタプルリストに基づいて処理を適用する関数。
-
-    Parameters:
-        df (pd.DataFrame): 処理対象のDataFrame。
-        groups (list): グループ化の基となる列名のリスト。
-        col2_func_tpl_list (list): ("left", "right", func) 形式のタプルのリスト。"left", "right"はdfの列名
-
-    Returns:
-        pd.DataFrame: 処理結果を含むDataFrame。
-    """
-
-    # グループ化して各関数を適用
     def apply_funcs(x):
         results = {}
         for left, right, func in col2_func_tpl_list:
             results[f'{left}-{right}_' + func.__name__] = func(x, left, right)
         return pd.Series(results)
+
     applied_df = df.groupby(groups).apply(apply_funcs)
 
     def remove_last_element(s):
         parts = s.split("_")
         return "_".join(parts[:-1])
+
     # applied_df = restore_column_dtypes(applied_df, dtypes_dict)
     if use_func_order:
         # new_columns = [f"apply2-agg_{str(inx)}" for inx, col in enumerate(applied_df.columns)]
@@ -179,12 +151,9 @@ def apply_agg_by_groups(df, groups, col2_func_tpl_list, use_func_order=False
         old_columns = applied_df.columns
         applied_df.columns = new_columns
         columns_map = dict(zip(new_columns, old_columns))
-        return (applied_df.reset_index(), columns_map)
+        return applied_df.reset_index(), columns_map
 
     return applied_df.reset_index()
-
-
-# def tf_by_groups(df, groups, tgt_names, func_dict, use_func_order=False):
 
 
 def agg_by_groups(df, groups, tgt_names, func_dict, use_func_order=False, is_debug=False):
@@ -202,7 +171,7 @@ def agg_by_groups(df, groups, tgt_names, func_dict, use_func_order=False, is_deb
         old_columns = agg_df.columns
         agg_df.columns = new_columns
         columns_map = dict(zip(new_columns, old_columns))
-        return (agg_df.reset_index(), columns_map)
+        return agg_df.reset_index(), columns_map
 
     # 結果を表示
     return agg_df.reset_index()
@@ -226,4 +195,4 @@ def tf_by_groups(df, groups, tgt_names, func_dict, use_func_order=False, is_debu
     if use_func_order:
         df_copy = df_copy.rename(columns=dict(zip(old_columns, new_columns)))
         columns_map = dict(zip(new_columns, old_columns))
-        return (df_copy, columns_map)
+        return df_copy, columns_map
